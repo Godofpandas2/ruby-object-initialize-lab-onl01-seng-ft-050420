6a68439ead665667e4cc913f91e16c3113636d7e
David Hutchison <dhutchis68516@gmail.com>
2020-05-12 18:10:27 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,9 @@
 class Dog
-  def initialize(name)
+  def initialize(name, breed = "Mutt")
     @name = name
+    @breed = breed
   end
-  attr_accessor :name
+  attr_accessor :name, :breed
 
 #  def initialize(breed = "Mutt")
 #    @breed = breed
